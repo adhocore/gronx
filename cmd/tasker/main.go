0d9940914f47af54b7e89b4c83b7a3703d47ec59
Jitendra Adhikari <jiten.adhikary@gmail.com>
2021-05-02 15:34:00 +0700

    feat(cmd.tasker): add tasker for standalone usage as task daemon

---
+++ cmd/tasker/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/adhocore/gronx/pkg/tasker"
+)
+
+func main() {
+	opt := mustGetOption()
+	taskr := tasker.New(opt)
+
+	for _, task := range tasker.MustParseTaskfile(opt) {
+		taskr.Task(task.Expr, tasker.Taskify(task.Cmd, opt))
+	}
+
+	if opt.Until > 0 {
+		taskr.Until(time.Duration(opt.Until) * time.Minute)
+	}
+
+	taskr.Run()
+}
+
+func mustGetOption() tasker.Option {
+	var opt tasker.Option
+
+	flag.StringVar(&opt.File, "file", "", "The task file in crontab format")
+	flag.StringVar(&opt.Tz, "tz", "Local", "The timezone to use for tasks")
+	flag.StringVar(&opt.Shell, "shell", tasker.Shell()[0], "The shell to use for running tasks")
+	flag.StringVar(&opt.Out, "out", "", "The fullpath to file where output from tasks are sent to")
+	flag.BoolVar(&opt.Verbose, "verbose", false, "The verbose mode outputs as much as possible")
+	flag.Int64Var(&opt.Until, "until", 0, "The timeout for task daemon in minutes")
+	flag.Parse()
+
+	if opt.File == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(opt.File); err != nil {
+		log.Fatalf("can't read taskfile: %s", opt.File)
+	}
+
+	return opt
+}
